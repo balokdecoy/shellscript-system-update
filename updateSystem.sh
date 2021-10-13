03734cdb55acc0f93e26ee35737fbc8b089a6d91
johnniesimpson <johnniesimpson@outlook.com>
2021-10-12 21:12:18 -0400

    Upload shell script file.

---
+++ updateSystem.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+sudo apt-get update
+echo Package list update complete!
+
+function yes_or_no {
+	read -p "$* [y/n] " yn
+		case $yn in
+			[Yy]*) echo 'See you in a sec...'; 
+				shutdown -r 0 ;;
+			[Nn]*) echo "Goodbye!" ; return 0 ;;
+			* ) echo "Please answer yes or no." ;;
+		esac
+}
+
+function upgrade_yn {
+	read -p "$* [y/n] " yn
+		case $yn in
+			[Yy]*) echo 'The system will now install updates...';
+				sudo apt-get upgrade ;
+				yes_or_no "Would you like to restart now?" ;;
+			[Nn]*) echo 'Updated package lists without installing.' ;
+				return 0 ;;
+			* ) echo 'Please answer yes or no.' ;;
+		esac
+}
+
+upgrade_yn "Would you like to install the updates?"
