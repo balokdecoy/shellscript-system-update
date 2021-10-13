24edd2f1ced63426c168f1b8ea72f93ffbeadee5
johnniesimpson <johnniesimpson@outlook.com>
2021-10-12 21:28:56 -0400

    Reordered functions in the shell for clarity.

--- updateSystem.sh
+++ updateSystem.sh
@@ -11,17 +11,6 @@ function update_yn {
 		esac
 }
 
-function yes_or_no {
-	read -p "$* [y/n] " yn
-		case $yn in
-			[Yy]*) echo 'See you in a sec...'; 
-				shutdown -r 0 ;;
-			[Nn]*) echo "Goodbye!" ; return 0 ;;
-			* ) echo "Please answer yes or no.";
-				yes_or_no "Would you like to restart now?" ;;
-		esac
-}
-
 function upgrade_yn {
 	read -p "$* [y/n] " yn
 		case $yn in
@@ -32,6 +21,17 @@ function upgrade_yn {
 				return 0 ;;
 			* ) echo 'Please answer yes or no.';
 				upgrade_yn "Would you like to install the updates?" ;;
+		esac
+}
+
+function yes_or_no {
+	read -p "$* [y/n] " yn
+		case $yn in
+			[Yy]*) echo 'See you in a sec...'; 
+				shutdown -r 0 ;;
+			[Nn]*) echo "Goodbye!" ; return 0 ;;
+			* ) echo "Please answer yes or no.";
+				yes_or_no "Would you like to restart now?" ;;
 		esac
 }
 
